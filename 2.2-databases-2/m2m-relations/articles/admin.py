66e66f786d7d27e1ae7e90ab607b665298e0034b
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-12-23 19:32:56 +1000

    database 2.2 updated

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from articles.models import Article, Tag, Scope
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main'] == True:
+                count +=1
+        if count != 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,19 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from school.models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-
+    teach_order = 'subject'
+    students = Student.objects.order_by(ordering)
+    teachers_all = Teacher.objects.order_by(teach_order)
+    context = {'object_list': students,
+               'teach_list': teachers_all}
     return render(request, template, context)
